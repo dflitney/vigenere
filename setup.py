6265a914f9d3a87a4a56ff075a5de604788a0455
Dave Flitney <dave.flitney@gmail.com>
2018-03-04 23:55:54 +0000

    Added a distutils setup.py file

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='Cypher',
+      version='1.0',
+      description='Cypher encryption package',
+      author='Dave Flitney',
+      author_email='dave.flitney@gmail.com',
+      packages=['cypher', ],
+      scripts=['cypher.py', ],
+      )
